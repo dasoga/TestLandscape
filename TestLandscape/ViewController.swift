99a357c6c3ad5c9a68aea4903bcfdc1b56e46279
Dante Solorio <ddasogaa@gmail.com>
2018-08-07 22:27:37 -0500

    Initial Commit

---
+++ TestLandscape/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestLandscape
+//
+//  Created by Dante Solorio on 8/7/18.
+//  Copyright © 2018 Dasoga Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
